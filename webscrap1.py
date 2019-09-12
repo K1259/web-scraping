36b52b9f091f18ca98dff1ddb69addbe9db83ae8
K1259 <55232784+K1259@users.noreply.github.com>
2019-09-12 14:32:36 +0530

    Add files via upload

---
+++ webscrap1.py
@@ -0,0 +1,41 @@
+from matplotlib import pyplot as plt
+from bs4 import BeautifulSoup
+import requests
+
+
+
+handle = input('Input your account name on Twitter: ')
+ctr = int(input('Input number of tweets to scrape: '))
+res=requests.get('https://twitter.com/'+ handle)
+bs=BeautifulSoup(res.content,"html.parser")
+all_tweets = bs.find_all('div',{'class':'tweet'})
+if all_tweets:
+ for tweet in all_tweets[:ctr]:
+ context = tweet.find('div',{'class':'context'}).text.replace("\n"," ").strip()
+ content = tweet.find('div', {'class': 'content'})
+ header = content.find('div',{'class':'stream-item-header'})
+ user = header.find('a',{'class':'account-group js-account-group js-action-profile js-user-profile-link js-nav'}).text.replace("\n"," ").strip()
+ time = header.find('a',{'class':'tweet-timestamp js-permalink js-nav js-tooltip'}).find('span').text.replace("\n"," ").strip()
+ message = content.find('div',{'class':'js-tweet-text-container'}).text.replace("\n"," ").strip()
+ footer = content.find('div',{'class':'stream-item-footer'})
+ stat = footer.find('div',{'class':'ProfileTweet-actionCountList u-hiddenVisually'}).text.replace("\n"," ").strip()
+ follow_box = bs.find('li', {'class': 'ProfileNav-item ProfileNav-item--followers'})
+ followers = follow_box.find('a').find('span', {'class': 'ProfileNav-value'})
+ if context:
+ print(context)
+ print(user,time)
+ print(message)
+ print(stat)
+
+else:
+ print("List is empty/account name not found.")
+p=format(followers.get('data-count'))
+
+
+print("Number of followers: {} ".format(followers.get('data-count')))
+plt.bar([50000000],[49844321 ],
+label=handle, color='red',width=.5)
+plt.xlabel('Name')
+plt.ylabel('Followers')
+plt.title('Information')
+plt.show()
